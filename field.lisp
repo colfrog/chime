de317c72557da80fc48383f9006765fa5b5c7695
Laurent Cimon <laurent@nilio.ca>
2023-12-26 15:07:42 -0500

    Fix rendering issues

--- field.lisp
+++ field.lisp
@@ -53,10 +53,10 @@
 (define-presentation-method present ((field field) (type field) stream (view board-view) &key)
   (with-slots (col row piece highlighted selected) field
     (let ((colour (if (evenp (+ row col)) +pale-goldenrod+ +dark-khaki+)))
-      (draw-rectangle* stream 0 0 49 49 :ink colour)
+      (draw-rectangle* stream 0 0 50 50 :ink colour)
       (when highlighted
-	(draw-rectangle* stream 0 0 49 49 :ink +red+ :filled nil :line-thickness 2))
+	(draw-rectangle* stream 1 1 49 49 :ink +red+ :filled nil :line-thickness 2))
       (when selected
-	(draw-rectangle* stream 0 0 49 49 :ink +black+ :filled nil :line-thickness 2))
+	(draw-rectangle* stream 1 1 49 49 :ink +black+ :filled nil :line-thickness 2))
       (when piece
 	(present piece 'piece :stream stream :view view)))))
