09a2d4e0bc6536c80aee41b6bd646fb42247f2a2
Laurent Cimon <laurent@nilio.ca>
2023-12-26 14:40:11 -0500

    Make the king's moves

--- field.lisp
+++ field.lisp
@@ -13,8 +13,42 @@
 	  :accessor piece)))
 
 (defmethod get-position ((field field))
-  (with-slots (col row) field
-    (cons col row)))
+  (with-slots (row col) field
+    (cons row col)))
+
+(defmethod positions-around (field)
+  (with-slots (row col) field
+    (let ((moves '()))
+      (dotimes (i 3)
+	(dotimes (j 3)
+	  (let ((other-row (+ row (1- i)))
+		(other-col (+ col (1- j))))
+	    (push (cons other-row other-col) moves))))
+      moves)))
+
+(defmethod is-checked ((field field) (board board) (colour string))
+  (with-slots (fields) board
+    (let ((attacks '()))
+      (dotimes (i 8)
+	(dotimes (j 8)
+	  (let* ((other-field (aref fields i j))
+		 (piece (piece other-field)))
+	    (when (and piece (string/= (colour piece) colour))
+	      (let ((piece-attacks
+		      (if (string= (kind piece) "king")
+			  (positions-around other-field)
+			  (if (string= (kind piece) "pawn")
+			      (let ((row (row other-field))
+				    (col (col other-field)))
+				(if (string= (colour piece) "white")
+				    (list (cons (1- row) (1+ col))
+					  (cons (1- row) (1- col)))
+				    (list (cons (1+ row) (1+ col))
+					  (cons (1+ row) (1- col)))))
+			      (possible-moves piece board)))))
+		(setf attacks (concatenate 'list attacks piece-attacks)))))))
+      (when (member (cons (row field) (col field)) attacks :test #'equal)
+	t))))
 
 (define-presentation-method present ((field field) (type field) stream (view board-view) &key)
   (with-slots (col row piece highlighted selected) field
--- pieces/king.lisp
+++ pieces/king.lisp
@@ -4,4 +4,19 @@
   ((kind :initform "king")))
 
 (defmethod possible-moves ((king king) (board board))
-  '())
+  (with-slots (fields) board
+    (with-slots (colour field) king
+      (let ((moves '()))
+	(dotimes (i 3)
+	  (dotimes (j 3)
+	    (let ((row (+ (row field) (1- i)))
+		  (col (+ (col field) (1- j))))
+	      (when (and (<= 0 row 7)
+			 (<= 0 col 7))
+		(let ((other-field (aref fields row col)))
+		  (when (and (not (is-checked other-field board colour))
+			     (or (and (piece other-field)
+				      (string/= (colour (piece other-field)) colour))
+				 (not (piece other-field))))
+		    (push (cons row col) moves)))))))
+	moves))))
